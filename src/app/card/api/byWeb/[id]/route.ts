43eaebfe75430e282754665d4077f02dbdd13435
Shusuke-Yamakawa <shusuke.y@aol.jp>
2024-03-11 13:24:32 +0900

    抽選用のお気に入りをデフォルトにする

--- src/app/card/api/byWeb/[id]/route.ts
+++ src/app/card/api/byWeb/[id]/route.ts
@@ -1,10 +1,10 @@
-import { favoriteAddReserve } from '@/src/app/card/api/byWeb/favoriteAdd';
+import { favoriteAddDraw } from '@/src/app/card/api/byWeb/favoriteAdd';
 
 export const dynamic = 'force-dynamic';
 
 export async function PUT(req: Request, { params }: { params: { id: string } }) {
-  // await favoriteAddDraw(params.id);
-  await favoriteAddReserve(params.id);
+  await favoriteAddDraw(params.id);
+  // await favoriteAddReserve(params.id);
 
   return new Response(JSON.stringify({ message: 'favoriteAddDraw' }), {
     headers: { 'Content-Type': 'application/json' },
