84c887ff25585475ee20e34054a5880a7fef776d
Shusuke-Yamakawa <shusuke.y@aol.jp>
2024-03-23 10:09:11 +0900

    定数を調整

--- src/app/batch/auto-reserved-new/auto-reserve.const.ts
+++ src/app/batch/auto-reserved-new/auto-reserve.const.ts
@@ -20,24 +20,29 @@ export const TARGET_COURT_SUB = [
     name: '府中の森公園',
     value: '1270',
   },
-  {
-    name: '武蔵野中央公園',
-    value: '1230',
-  },
+  // {
+  //   name: '武蔵野中央公園',
+  //   value: '1230',
+  // },
 ] as const satisfies Court[];
 
-export const EXCLUDE_DAY_LIST: number[] = [6, 7, 20];
+export const EXCLUDE_DAY_LIST: number[] = [];
 
 export const DESIRED_RESERVATION_DATE_LIST: number[] = [];
 
-// export const USER_ID = '10002097';
-// export const PASSWD = 'Ryouma2518';
+// 10000973 Aa00620513
+// 10002865 I-am-always-hangover
+// 10003498 Oomiya0911
+// 10004025 wiHpar-joxrym-xazqy9 yamamoto
+// 10002097 Ryouma2518
+// 10001197 Tennis0503
+// 10001371 Agoago0223
+// 10001498 hagayuk01!
+// 10003974 Cycling0818@
+// 10004678 4uFgP7JZLg kuma
 
-export const USER_ID = '10001197';
-export const PASSWD = 'Tennis0503';
-
-// export const USER_ID = '10003974';
-// export const PASSWD = 'Cycling0818@';
+export const USER_ID = '10002865';
+export const PASSWD = 'I-am-always-hangover';
 
 export const RETRY_USER_ID = '10001498';
 export const RETRY_PASSWD = 'hagayuk01!';
